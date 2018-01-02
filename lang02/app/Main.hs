bc4c299934ae2cabe0ebb967a19ea1f3de5bca4a
Yusuke Hosonuma <tobi462@gmail.com>
2018-01-02 10:08:28 +0900

    refactor: use concatMap

--- lang02/app/Main.hs
+++ lang02/app/Main.hs
@@ -3,8 +3,8 @@ module Main where
 import Lib
 
 f :: String -> String -> String
-f x y = concat $ map (\(x, y) -> x : [y]) $ zip x y
+f x y = concatMap (\(x, y) -> x : [y]) $ zip x y
 
 main :: IO ()
-main = 
+main =
     putStr $ f "パトカー" "タクシー"
